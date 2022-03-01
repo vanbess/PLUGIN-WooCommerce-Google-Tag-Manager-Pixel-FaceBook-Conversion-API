895183734d1a78573531525ef8bb4c6e7b2193ce
vanbess <dev@silverbackdev.co.za>
2022-03-01 14:35:48 +0200

    Added check for presence of Google Tag Manager for WordPress plugin

--- index.php
+++ index.php
@@ -11,20 +11,37 @@ if (!defined('ABSPATH')) :
     exit();
 endif;
 
-// constants
-if (!defined('SBWC_GTMP_PATH')) :
-    define('SBWC_GTMP_PATH', plugin_dir_path(__FILE__));
+// if Google Tag Manager for WordPress not present, display error message and don't load plugin files
+if (!defined('GTM4WP_PATH')) :
+
+    function sbwc_gtmp_gtm4wp_error()
+    { ?>
+        <div class="notice notice-error is-dismissible">
+            <p><?php _e('<b>Google Tag Manager for WordPress needs to be installed and activated in order for SBWC Google Tag Manager (Pixel) & FB Conversion API Settings plugin to work.</b>', 'default'); ?></p>
+        </div>
+<?php }
+
+    add_action('admin_notices', 'sbwc_gtmp_gtm4wp_error');
+
+// if Google Tag Manager for WordPress present, load plugin files
+elseif (defined('GTM4WP_PATH')) :
+
+    // constants
+    if (!defined('SBWC_GTMP_PATH')) :
+        define('SBWC_GTMP_PATH', plugin_dir_path(__FILE__));
+    endif;
+
+    if (!defined('SBWC_GTMP_URL')) :
+        define('SBWC_GTMP_URL', plugin_dir_url(__FILE__));
+    endif;
+
+    // init
+    add_action('plugins_loaded', 'sbwc_gtmp_init');
+
+    function sbwc_gtmp_init()
+    {
+        // core class
+        include SBWC_GTMP_PATH . 'class/SBWC_GTMP_Admin.php';
+    }
+
 endif;
-
-if (!defined('SBWC_GTMP_URL')) :
-    define('SBWC_GTMP_URL', plugin_dir_url(__FILE__));
-endif;
-
-// init
-add_action('plugins_loaded', 'sbwc_gtmp_init');
-
-function sbwc_gtmp_init()
-{
-    // core class
-    include SBWC_GTMP_PATH . 'class/SBWC_GTMP_Admin.php';
-}
